b8ad5730aaedd11fad7a2ffcf1d9feca41073f4a
Wayne-Thornberry <way-net@outlook.com>
2022-11-04 20:57:59 +0000

    Init server

---
+++ code/OnlineServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using Proline.ServerAccess.IO.Actions;
+using System;
+
+namespace OnlineEngine
+{
+    public class Program
+    {
+        private static ConsoleWriteAction _x;
+        private static ReadFileAction _y;
+        private static WriteFileAction _g;
+        private static ConsoleWriteLineAction _a;
+
+        public static void Main(string[] args)
+        { 
+            try
+            {
+                _x = new ConsoleWriteAction();
+                _x.SubscribeEvent();
+                _a = new ConsoleWriteLineAction();
+                _a.SubscribeEvent();
+                _y = new ReadFileAction();
+                _y.SubscribeEvent();
+                _g = new WriteFileAction();
+                _g.SubscribeEvent();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ code/Proline.ServerAccess.IO/ReadFileAction.cs
@@ -0,0 +1,77 @@
+﻿using CitizenFX.Core;
+using Newtonsoft.Json;
+using Proline.Resource.Eventing;
+using Proline.ServerAccess.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Console = Proline.Resource.Console;
+
+namespace Proline.ServerAccess.IO.Actions
+{
+    public class ReadFileAction : ExtendedEvent
+    {
+        private static ReadFileAction _event;
+
+        public string Data { get; set; }
+
+        public ReadFileAction() : base(EventNameDefinitions.LoadFileHandler, true)
+        {
+
+        }
+        public void SubscribeEvent()
+        {
+            if (_event == null)
+            {
+                _event = new ReadFileAction();
+                _event.Subscribe();
+            }
+        }
+
+        public void UnsubscribeEvent()
+        {
+            if (_event != null)
+            {
+                _event.Unsubscribe();
+                _event = null;
+            }
+        } 
+
+        protected override object OnEventTriggered(Player player, params object[] args)
+        {
+            try
+            { 
+                // old way via getting id
+                if (args.Length > 0)
+                { 
+                    var argPath = args[0].ToString();
+                    var LocalPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                    var DEFAULT_PATH = Path.Combine(LocalPath, "ProjectOnline");
+                    var newPath = Path.Combine(DEFAULT_PATH, argPath);
+
+                    string fileAndPath = newPath;
+                    string currentDirectory = Path.GetDirectoryName(fileAndPath);
+                    string fullPathOnly = Path.GetFullPath(currentDirectory);
+
+                    if (!Directory.Exists(fullPathOnly))
+                        return null;
+
+                    var data = File.ReadAllText(newPath);
+                    return data;
+                }
+                else
+                {
+                    Console.WriteLine("Argument count does not match expected count"); 
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return null;
+        } 
+    }
+}
