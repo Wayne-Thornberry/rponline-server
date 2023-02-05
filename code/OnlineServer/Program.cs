5b0493ad3582a59b542e176e7742bfc02a4fc553
Wayne-Thornberry <way-net@outlook.com>
2023-02-05 18:28:21 +0000

    :)

--- code/OnlineServer/Program.cs
+++ code/OnlineServer/Program.cs
@@ -1,27 +1,24 @@
-﻿using Proline.ServerAccess.IO.Actions;
-using System;
+﻿using System; 
+using System.Collections.Generic;
+using Proline.Resource.Eventing; 
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Proline.ClassicOnline.EventQueue;
 
 namespace OnlineEngine
 {
     public class Program
-    {
-        private static ConsoleWriteAction _x;
-        private static ReadFileAction _y;
-        private static WriteFileAction _g;
-        private static ConsoleWriteLineAction _a;
-
+    {  
         public static void Main(string[] args)
-        { 
+        {
+            Console.WriteLine($"Started Server");
             try
             {
-                _x = new ConsoleWriteAction();
-                _x.SubscribeEvent();
-                _a = new ConsoleWriteLineAction();
-                _a.SubscribeEvent();
-                _y = new ReadFileAction();
-                _y.SubscribeEvent();
-                _g = new WriteFileAction();
-                _g.SubscribeEvent();
+                Component.InitializeComponents();
             }
             catch (Exception e)
             {
--- deploy.ps1
+++
@@ -1,6 +0,0 @@
-﻿#Remove-Item "D:\ProjectOnline\resources\server-core\*"  -Recurse
-#Copy-Item -Path ".\vendor\ProjectOnline\*" -Destination "D:\ProjectOnline\resources\server-core\" -Recurse
-Copy-Item -Path ".\data\*" -Destination "D:\ProjectOnline\resources\server-core\" -Recurse 
-Copy-Item -Path ".\artifacts\*" -Destination "D:\ProjectOnline\resources\server-core\" -Recurse
-Remove-Item "D:\ProjectOnline\resources\server-core\CitizenFX.Core.*.dll" -Recurse
-Remove-Item "D:\ProjectOnline\resources\server-core\*.pdb" -Recurse
